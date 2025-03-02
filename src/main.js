6dc290f15edf6bdc85772cba89fff8f75fd029f1
Ba49 <ba.nerriere@gmail.com>
2025-03-02 23:18:37 +0100

    another one

--- src/main.js
+++ src/main.js
@@ -55,7 +55,7 @@ function init() {
 
     const loader = new GLTFLoader();
     loader.setDRACOLoader( dracoLoader );
-    loader.setPath( './Assets/' );
+    loader.setPath( 'Assets/' );
     loader.load( 'SM_Bolero_R01_Low.glb', function ( gltf ) {
 
       const fauteuil = gltf.scene;
